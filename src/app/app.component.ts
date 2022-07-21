678eb43e67e492ee7c27b4e119766cbb8ce2db39
Felippe Negrão de Oliveira <felippenegrao@alunos.utfpr.edu.br>
2022-07-21 11:15:03 -0300

    showing information on screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank-angular';
+  destino: number = 0;
+  valor: number = 0;
 
-  transferir($event: any){
-console.log($event)
+  transferir($event: any) {
+    console.log($event);
+    this.destino = $event.destino;
+    this.valor = $event.valor;
   }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -6,7 +6,6 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
-
   @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number = 0;
@@ -16,6 +15,12 @@ export class NovaTransferenciaComponent {
     console.log('Nova Transferência');
     console.log(this.valor);
     console.log(this.destino);
-    this.aoTransferir.emit({valor: this.valor, destino: this.destino})
+    this.aoTransferir.emit({ valor: this.valor, destino: this.destino });
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
   }
 }
